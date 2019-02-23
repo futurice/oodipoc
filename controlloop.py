365c715fac5444808f2c67c6789d4bd662dd9bfc
Teemu Turunen <teemu.turunen@gmail.com>
2019-02-23 16:08:44 +0200

    basic auth hidden file, placeholders for mir mission management calls

--- controlloop.py
+++ controlloop.py
@@ -54,8 +54,38 @@ def find_position_by_category(category):
     for row in rows:
         return row[0]
 
+def get_mir_auth():
+    # read the basic auth token off a file outside this repository
+    mir_auth = open("../.mir_auth").readline()
+    # readline adds a newline to the end, so strip that
+    mir_auth = mir_auth.rstrip('\n')
+    return mir_auth
+
 def get_mir_status():
-    # doesn't work yet 
+    
+    command = 'curl -sb GET "http://mir.com/api/v2.0.0/status" -H "accept: application/json" -H "' + get_mir_auth() + '" -H "Accept-Language: en_US" | egrep "state_text" | tr -d " " | cut -d\'"\' -f4'
+    print(command)
+#   state = subprocess.run(command)
+#   print("State: " + state)
+
+def get_mir_mission_details():
+    # todo, get details for our move-to-location mir mission via curl call to rest api
+    print("get_mir_mission_details called")
+
+def delete_mir_action(guid):
+    # todo, delete a certain action from our move-to-location mir mission via curl call to rest api
+    print("delete_mir_action called with guid: " + guid)
+
+def add_mir_action(location):
+    # todo, add a move action to a defined location into our move-to-location mir mission via curl call to rest api
+    print("add_mir_action called with location: " + location)
+
+def modify_mir_mission(location):
+    # todo make use of the mir mission modifying calls to setup our move-to-location mir mission to match the book/category location
+    print("modify mission called with location: " + location)
+    get_mir_mission_details()
+    delete_mir_action("1")
+    add_mir_action(location)
 
 def main():
 
@@ -67,9 +97,7 @@ def main():
             category = f.readline()
             position = str(find_position_by_category(category))
             print("Position for category " + category + " is " + position) 
-                        # TODO: create a mission with a move to this position and back to homebase (and other actions)
-                        # TODO: requires a mission template and some testing and calls to the REST API
-#           get_mir_status()
+            modify_mir_mission(position)
 
         except IOError:
             print("Waiting for a mission ...", file=sys.stderr)
