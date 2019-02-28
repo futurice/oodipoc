7835c399389f9e20c9d4e7de76631c0dbd9b5adc
Teemu Turunen <teemu.turunen@gmail.com>
2019-02-28 09:31:39 +0200

    converted the curl crap into using json and requests

--- mir_calls.py
+++ mir_calls.py
@@ -9,6 +9,8 @@ from __future__ import print_function
 
 import sys
 import subprocess
+import json
+import requests
 
 def get_mir_auth():
     # read the basic auth token off a file outside this repository
@@ -17,25 +19,107 @@ def get_mir_auth():
     mir_auth = mir_auth.rstrip('\n')
     return mir_auth
 
+def get_headers():
+    auth = get_mir_auth()
+    headers = {'accept': 'application/json', 'Authorization': auth, 'Accept-Language': 'en_US', 'Content-Type': 'application/json'}
+    return headers
+
 def get_mir_status():
+
     # get the current status from the mir robot 
-    command = 'curl -sb GET "http://mir.com/api/v2.0.0/status" -H "accept: application/json" -H "' + get_mir_auth() + '" -H "Accept-Language: en_US" | egrep "state_text" | tr -d " " | cut -d\'"\' -f4'
-    print(command)
-    #state = subprocess.run(command)
-    #print("State: " + state)
+
+    url = 'http://mir.com/api/v2.0.0/status'
+    headers = get_headers()
+
+    response = requests.get(url, headers=headers)
+    json_data = json.loads(response.text)
+    return json_data["state_text"]
 
 def delete_mir_move_action():
+
     # delete a certain action from our move-to-location mir mission via curl call to rest api
-    command = 'curl -sb DELETE "http://mir.com/api/v2.0.0/missions/2e066786-3424-11e9-954b-94c691a3a93e/actions/11111111-1111-1111-1111-111111111111" -H "accept: application/json" -H "' + get_mir_auth() + '" -H "Accept-Language: en_US"'
-    print(command)
 
-def add_mir_move_action(position):
+    url = 'http://mir.com/api/v2.0.0/missions/2e066786-3424-11e9-954b-94c691a3a93e/actions/11111111-1111-1111-1111-111111111111'
+    headers = get_headers()
+    response = requests.delete(url, headers=headers)
+
+def delete_mir_reaction_sound_action():
+
+    # delete a certain action from our move-to-location mir mission via curl call to rest api
+
+    url = 'http://mir.com/api/v2.0.0/missions/d6184d0f-3a94-11e9-b2d2-94c691a3a93e/actions/11111111-1111-1111-1111-111111111112'
+    headers = get_headers()
+    response = requests.delete(url, headers=headers)
+
+def add_mir_move_action(position = "0b676baa-3423-11e9-954b-94c691a3a93e"):
+
     # add a move action to a defined location into our move-to-location mir mission via curl call to rest api
-    # placeholder until the position guid mapping logic is there 
-    position = "0b676baa-3423-11e9-954b-94c691a3a93e"
 
-    command = 'curl -sb POST "http://mir.com/api/v2.0.0/missions/2e066786-3424-11e9-954b-94c691a3a93e/actions" -H "accept: application/json" -H "' + get_mir_auth() + '" -H "Accept-Language: en_US" -H "Content-Type: application/json" -d "{ \"action_type\": \"move\", \"mission_id\": \"2e066786-3424-11e9-954b-94c691a3a93e\", \"guid\": \"11111111-1111-1111-1111-111111111111\", \"parameters\": [ { \"guid\": \"2e9e391a-3424-11e9-954b-94c691a3a93e\", \"id\": \"position\", \"input_name\": null, \"value\": \"' + position + '\" }, { \"guid\": \"2ea2b83c-3424-11e9-954b-94c691a3a93e\", \"id\": \"retries\", \"input_name\": null, \"value\": 1 }, { \"guid\": \"2ea788d7-3424-11e9-954b-94c691a3a93e\", \"id\": \"distance_threshold\", \"input_name\": null, \"value\": 0.1 } ], \"priority\": 1, \"url\": \"/v2.0.0/mission_actions/2e9758a2-3424-11e9-954b-94c691a3a93e\" }"'
-    print(command)
+    url = "http://mir.com/api/v2.0.0/missions/2e066786-3424-11e9-954b-94c691a3a93e/actions"
+    headers = get_headers()
+    data = {
+    "action_type": "move", 
+    "guid": "11111111-1111-1111-1111-111111111111", 
+    "mission_id": "2e066786-3424-11e9-954b-94c691a3a93e", 
+    "parameters": [ 
+      { 
+        "id": "position", 
+        "value": "0b676baa-3423-11e9-954b-94c691a3a93e" 
+      }, 
+      { 
+        "id": "retries", 
+        "value": 1  
+      }, 
+      { 
+         "id": "distance_threshold", 
+         "value": 0.1 
+      } 
+    ], 
+
+    "priority": 1, 
+    "url": "/v2.0.0/mission_actions/2e9758a2-3424-11e9-954b-94c691a3a93e"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+def add_mir_reaction_sound(sound = "45f02254-d6a5-11e8-8995-94c691a3e21e", priority = 1, volume = 100):
+
+   # prioriy = which action within the mission
+   # volume = 1-100
+   # woops = 45f02254-d6a5-11e8-8995-94c691a3e21e
+   # ticketytick = 6ee40f76-3aac-11e9-800b-94c691a3a93e
+
+    url = "http://mir.com/api/v2.0.0/missions/d6184d0f-3a94-11e9-b2d2-94c691a3a93e/actions"
+    headers = get_headers()
+
+    data = {
+    "action_type": "sound",
+    "guid": "11111111-1111-1111-1111-111111111112",
+    "mission_id": "d6184d0f-3a94-11e9-b2d2-94c691a3a93e",
+    "parameters": [
+      {
+        "id": "sound",
+        "value": sound
+      },
+      {
+        "id": "volume",
+        "value": volume
+      },
+      {
+        "id": "mode",
+        "value": "full"
+      },
+      {
+        "id": "duration",
+        "value": "00:00:01.000000"
+      }
+    ],
+
+    "priority": priority,
+    "url": "/v2.0.0/mission_actions/d6184d0f-3a94-11e9-b2d2-94c691a3a93e"
+    }
+
+    response = requests.post(url, headers=headers, json=data)
 
 def modify_mir_mission(position):
     # todo make use of the mir mission modifying calls to setup our move-to-location mir mission to match the book/category location
