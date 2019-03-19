c0655d83d97986f529c82e78c38f5eb3e1afd433
minjaaxelsson <minjaaxelsson@gmail.com>
2019-03-19 14:43:34 +0200

    add lookleft, lookright, lookdown, lookup, roolleyes and toproll to eyes.py

--- eyes.py
+++ eyes.py
@@ -3,12 +3,9 @@
 import serial
 import sys
 import time
-#from common import clock, draw_str
 
-#or should baud be 9600?
-
-#ser = serial.Serial("/dev/cu.usbmodem1441",baudrate=19200, timeout = 1)
 global ser
+#remember to check if port is correct
 ser = serial.Serial("/dev/cu.usbmodem1441",baudrate=19200, timeout = 1)
 
 def lookLeft():
@@ -18,26 +15,52 @@ def lookLeft():
     #ser.close()
 
 def lookRight():
-    ser.write(bytes("-200", encoding="ascii"))
+    ser.write(bytes("-800", encoding="ascii"))
     print("I looked right")
     time.sleep(2)
     #ser.close()
 
-def eyesTest(): 
+def lookDown():
+    ser.write(bytes("0", encoding="ascii"))
+    print("I looked down")
+    time.sleep(2)
+
+def lookUp():
+    ser.write(bytes("1600", encoding="ascii"))
+    print("I looked up")
+    time.sleep(2)
+
+def rollEyes():
+    ser.write(bytes("5000", encoding="ascii"))
+    print("I looked up")
+    time.sleep(2)
+    ser.write(bytes("0", encoding="ascii"))
+    time.sleep(2)
+
+def topRoll():
+    ser.write(bytes("1900", encoding="ascii"))
+    time.sleep(2)
+    ser.write(bytes("1300", encoding="ascii"))
+    time.sleep(2)
+    ser.write(bytes("1900", encoding="ascii"))
+    time.sleep(2)
+    ser.write(bytes("1300", encoding="ascii"))
+    time.sleep(2)
+
+def eyesInput(): 
     arduinoWrite = input("give me a number:")
     print(arduinoWrite)
-    #print(type(arduinoWrite))
     ser.write(bytes(arduinoWrite, encoding="ascii"))
     print("sent data")
 
+#main loop for testing
 def main():
     time.sleep(2)
-    #direction = str("-800")
-    #print(type(direction))
-    #ser.write(bytes(direction, encoding="ascii"))
-    lookLeft()
-    print("I did the function")
-    lookRight()
+    #lookLeft()
+    #lookRight()
+    #lookUp()
+    #lookDown()
+    rollEyes()
 
 
 if __name__ == "__main__":
